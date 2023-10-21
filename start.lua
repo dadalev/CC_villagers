cf2e419fa8415786803ec9893ff49c52695364a0
dadalev <dadalev-9@seznam.cz>
2023-10-21 22:56:38 +0200

    Add files via upload

--- start.lua
+++ start.lua
@@ -1,7 +1,7 @@
 print("Starting")
 
 function run()
-    rm setup.lua
-    wget https://raw.githubusercontent.com/dadalev/CC_villagers/main/setup.lua
-    setup.lua
+    fs.delete(setup.lua)
+    shell.run(wget https://raw.githubusercontent.com/dadalev/CC_villagers/main/setup.lua)
+    shell.run(setup.lua)
 end
